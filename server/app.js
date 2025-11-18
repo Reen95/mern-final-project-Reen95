4e466c9a5743ee41fd0ddc959c49d780840b7323
Reen <maureenmukami531@gmail.com>
2025-11-18 18:40:06 +0300

    Initialize Express server with CORS and routes

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => res.json({ message: 'MERN capstone API is up' }));
+
+module.exports = app;
